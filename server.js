acd6dd271daaf8ea63f176ff8f92dd2190a8b86a
Hugo Velazquez <electrorokers@gmail.com>
2016-07-14 05:17:13 -0500

    Lab06 ConfigFile Se carganconfiguraciones de un json

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 // http
  var http = require("http");
  var fs = require('fs');
- / Obteniendo información del entorno
- // De ejecución con respecto al IP
- // y al puerto que debemos usar en
- // nuestro server.
- var PORT = process.env.PORT || 3000;
- var IP = process.env.IP || '127.0.0.1';
- if(IP=='127.0.0.1'){
-     console.log("> ---- EJECUTANDO EN MODO LOCAL ----");
+ var config = require("./config/config.js");
+ // Obteniendo las configuraciones
+ // del modulo de configuracion
+ var PORT = config.PORT;
+ var IP = config.IP;
+ if(IP == '127.0.0.1'){
+      console.log("> ---- EJECUTANDO EN MODO LOCAL ----");
  }
  // Crear un servidor basico
  var server = http.createServer(function(req, res){
